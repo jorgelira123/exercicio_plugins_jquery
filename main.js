13fb8a186b57e8b9b953cb5066df6ac1505e95dc
Jorge Lira <jorgelira542502@gmail.com>
2023-06-29 14:22:33 -0300

    Entrega ex 10, commit - 1

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    
+    $('#telefone-cadastro').mask('(00) 00000-0000')
+    $('#cpf-cadastro').mask('000.000.000-00')
+    $('#cep-cadastro').mask('00000-000')
+
+})
